6b9d06bfe91f03da27bb38da12d049c4f7dc4353
Ray Donnelly <mingw.android@gmail.com>
2017-09-15 06:56:29 -0700

    Fix macOS build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,19 @@
 set -e
 set -x
 
-./configure --prefix="$PREFIX" --disable-install-doc --enable-load-relative \
-  --enable-shared --with-openssl-dir="$PREFIX" --with-readline-dir="$PREFIX" \
-  --with-tcl-dir="$PREFIX" --with-tk-dir="$PREFIX" --with-libyaml-dir="$PREFIX" \
-  --with-zlib-dir="$PREFIX"
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LDFLAGS="${LDFLAGS_CC} -Wl,-rpath,${PREFIX}/lib"
+fi
+
+./configure --prefix="${PREFIX}"             \
+            --disable-install-doc            \
+            --enable-load-relative           \
+            --enable-shared                  \
+            --with-openssl-dir="${PREFIX}"   \
+            --with-readline-dir="${PREFIX}"  \
+            --with-tcl-dir="${PREFIX}"       \
+            --with-tk-dir="${PREFIX}"        \
+            --with-libyaml-dir="${PREFIX}"   \
+            --with-zlib-dir="${PREFIX}"
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
