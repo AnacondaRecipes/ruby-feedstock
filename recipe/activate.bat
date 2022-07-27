ff7ea780b19ddca713c4916a45f63b8a5dce3014
Kai Tietz <ktietz@anaconda.com>
2022-07-27 21:22:10 +0200

    update 3.1.2

---
+++ recipe/activate.bat
@@ -0,0 +1,3 @@
+@echo off
+set GEM_HOME=%CONDA_PREFIX%\share\rubygems
+set PATH=%CONDA_PREFIX%\share\rubygems\bin;%PATH%
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,82 @@
 #!/bin/bash
-
 # Get an updated config.sub and config.guess
-cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./tool
-
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./ext/fiddle/libffi-3.2.1
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./tool
 set -e
 set -x
 
-./configure --prefix="${PREFIX}"             \
-            --disable-install-doc            \
-            --enable-load-relative           \
-            --enable-shared                  \
-            --with-openssl-dir="${PREFIX}"   \
-            --with-readline-dir="${PREFIX}"  \
-            --with-tcl-dir="${PREFIX}"       \
-            --with-tk-dir="${PREFIX}"        \
-            --with-libyaml-dir="${PREFIX}"   \
-            --with-zlib-dir="${PREFIX}"
-make -j${CPU_COUNT} ${VERBOSE_AT}
+export CC=$(basename $CC)
+export CPP=$(basename $CPP)
+export CXX=$(basename $CXX)
+export STRIP=$(basename $STRIP)
+export NM=$(basename $NM)
+export OBJDUMP=$(basename $OBJDUMP)
+export AS=$(basename $AS)
+export AR=$(basename $AR)
+export RANLIB=$(basename $RANLIB)
+export LD=$(basename $LD)
+
+# Remove vendored libffi
+rm -rf ext/fiddle/libffi-3.2.1
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
+  (
+    mkdir -p build-host
+    pushd build-host
+
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CXXFLAGS
+
+    # --enable-shared \
+    ../configure \
+      --host=$BUILD \
+      --prefix="$BUILD_PREFIX" \
+      --disable-install-doc \
+      --enable-load-relative \
+      --with-libffi-dir="$BUILD_PREFIX" \
+      --with-libyaml-dir="$BUILD_PREFIX" \
+      --with-openssl-dir="$BUILD_PREFIX" \
+      --with-readline-dir="$BUILD_PREFIX" \
+      --with-zlib-dir="$BUILD_PREFIX"
+
+    make -j ${CPU_COUNT}
+    make install
+  )
+fi
+
+autoconf
+
+./configure \
+  --prefix="${PREFIX}" \
+  --disable-install-doc \
+  --enable-load-relative \
+  --enable-shared \
+  --with-libffi-dir="$PREFIX" \
+  --with-libyaml-dir="$PREFIX" \
+  --with-openssl-dir="$PREFIX" \
+  --with-readline-dir="$PREFIX" \
+  --with-zlib-dir="$PREFIX"
+
+make -j ${CPU_COUNT}
+# make check works locally on Linux, but not on CI Nodes, issue seems related to IPv6 and closed ports
+# make check
 make install
+
+mkdir -p $PREFIX/etc
+mkdir -p $PREFIX/share/rubygems/
+
+echo "gemhome: ${PREFIX}/share/rubygems" > $PREFIX/etc/gemrc
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/${CHANGE}.ps1" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.ps1"
+done
