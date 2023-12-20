371ea25e580b7f99509a8da049e42498c3f2b8ad
Serhii Kupriienko <skupriienko@anaconda.com>
2023-12-20 11:28:46 +0200

    ruby 3.2.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./ext/fiddle/libffi-3.2.1
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./tool
 set -e
 set -x
@@ -16,8 +15,6 @@ export AR=$(basename $AR)
 export RANLIB=$(basename $RANLIB)
 export LD=$(basename $LD)
 
-# Remove vendored libffi
-rm -rf ext/fiddle/libffi-3.2.1
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
   (
